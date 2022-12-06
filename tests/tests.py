cf5e5dff68c5d292bce1a660b34cb72b5e997fe3
Kévin Dérécusson <derec84@hotmail.fr>
2022-12-06 17:20:33 +0100

    Add urls and views testing for home page

---
+++ tests/tests.py
@@ -0,0 +1,25 @@
+from django.test import Client
+from django.urls import reverse, resolve
+from pytest_django.asserts import assertTemplateUsed
+
+
+def test_index_url():
+    path = reverse("index")
+    assert path == "/"
+    assert resolve(path).view_name == "index"
+
+
+def test_index_view():
+    client = Client()
+    path = reverse("index")
+    profiles_path = reverse("profiles_index")
+    lettings_path = reverse("lettings_index")
+    response = client.get(path)
+    content = response.content.decode()
+    expected_title = "<title>Holiday Homes</title>"
+
+    assert response.status_code == 200
+    assert expected_title in content
+    assert profiles_path in content
+    assert lettings_path in content
+    assertTemplateUsed(response, "index.html")
